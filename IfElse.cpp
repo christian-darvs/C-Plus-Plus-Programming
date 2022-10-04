2eb360ec73c8dc1cc8610c98d8d867fb8702ac85
Christian Darvin <91941006+christian-darvs@users.noreply.github.com>
2022-10-05 01:36:14 +0800

    Create IfElse.cpp

---
+++ IfElse.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int age = 18;
+  if (age >= 18){
+    cout<<"You are allowed to enter";
+  }else{
+    cout<<"You are not allowed to enter";
+  }
+   return 0;
+}
